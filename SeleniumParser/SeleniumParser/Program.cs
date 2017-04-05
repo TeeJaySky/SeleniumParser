2e7533cae027814f4472fdb0dd48168f493f392b
TeeJaySky <trentjsky@gmail.com>
2017-04-05 23:23:54 +1000

    Introducing bare bones visual studio project that has downloaded selenium web driver via the nuget package manager. Main method opens chromedriver.exe which I had to manually download (note v2.9 did not work so am using v2.25)

---
+++ SeleniumParser/SeleniumParser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Users\Trent\Desktop\Projects\SeleniumParser\SeleniumParser");
+            driver.Url = "http://www.amazon.com";
+
+            Console.ReadKey();
+        }
+    }
+}
